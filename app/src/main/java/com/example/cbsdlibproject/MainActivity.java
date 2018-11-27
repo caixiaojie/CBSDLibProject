54cff0fd41a7db0675014e3b2ecd843b999a2467
二哈 <1256347250@qq.com>
2018-11-27 11:10:59 +0800

    测试签名（get post请求写好 待测试）

--- app/src/main/java/com/example/cbsdlibproject/MainActivity.java
+++ app/src/main/java/com/example/cbsdlibproject/MainActivity.java
@@ -2,12 +2,80 @@ package com.example.cbsdlibproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.cbsdlib.nets.callbacks.AbsAPICallback;
+import com.example.cbsdlib.nets.example.CBSDService;
+import com.example.cbsdlib.nets.example.bean.DetailRespBean;
+import com.example.cbsdlib.nets.example.bean.UserRequestBean;
+import com.example.cbsdlib.nets.example.bean.UserRespBean;
+import com.example.cbsdlib.nets.exceptions.ApiException;
+import com.example.cbsdlib.ui.activities.BaseActivity;
+import com.example.cbsdlib.utils.WechantSign;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+import rx.Observer;
+
+public class MainActivity extends BaseActivity {
+    @BindView(R.id.btn)
+    Button btn;
+    @BindView(R.id.btn1)
+    Button btn1;
+    private UserRequestBean userRequestBean;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected int getContentView() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void init(Bundle saveInstanceState) {
+        userRequestBean = new UserRequestBean();
+        userRequestBean.setFieldName("111");
+        userRequestBean.setFieldValue("123456");
+        userRequestBean.setSort("1");
+//        userRequestBean.setNonce_str(WechantSign.create_nonce_str());
+//        userRequestBean.setTime_str(WechantSign.create_timestamp());
+//        Map<String, Object> map = new HashMap<>();
+//        map = WechantSign.objectToMap(userRequestBean);
+//        String sign = WechantSign.getSign(map, "123");
+//        userRequestBean.setSign(sign);
+    }
+    @OnClick({R.id.btn,R.id.btn1})
+    public void click(View view) {
+        switch (view.getId()) {
+            case R.id.btn:
+                CBSDService.getInstance().userLogin(userRequestBean).subscribe(new AbsAPICallback<UserRespBean>() {
+                    @Override
+                    public void onNext(UserRespBean userRespBean) {
+
+                    }
+
+                    @Override
+                    protected void onResultError(ApiException ex) {
+
+                    }
+                });
+                break;
+            case R.id.btn1:
+                CBSDService.getInstance().detail().subscribe(new AbsAPICallback<DetailRespBean>() {
+                    @Override
+                    public void onNext(DetailRespBean userRespBean) {
+
+                    }
+
+                    @Override
+                    protected void onResultError(ApiException ex) {
+
+                    }
+                });
+                break;
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/cbsdlibproject/PhonePwdLoginResultVo.java
@@ -0,0 +1,17 @@
+package com.example.cbsdlibproject;
+
+/**
+ * Created by Administrator on 2017/7/20 0020.
+ */
+
+public class PhonePwdLoginResultVo {
+    private String session_id;
+
+    public String getSession_id() {
+        return session_id;
+    }
+
+    public void setSession_id(String session_id) {
+        this.session_id = session_id;
+    }
+}
